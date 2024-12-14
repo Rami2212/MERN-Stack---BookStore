199afd022a3fb09c98c74e6e471b4cdef1fabd6f
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2024-12-14 23:42:33 +0530

    get one book by id

--- backend/index.js
+++ backend/index.js
@@ -60,6 +60,22 @@ app.get('/books', async (request, response) => {
     }
 })
 
+//route to get one book from database
+app.get('/books/:id', async (request, response) => {
+    try {
+
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
