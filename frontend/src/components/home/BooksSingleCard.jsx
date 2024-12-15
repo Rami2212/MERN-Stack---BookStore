a2deb8f512c0ccf88fe2e82a0db0b043f768061c
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2024-12-15 17:43:26 +0530

    single book card added

---
+++ frontend/src/components/home/BooksSingleCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { PiBookOpenTextLight } from 'react-icons/pi'
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BiUserCircle, BiInfoCircle } from 'react-icons/bi';
+import { MdOutlineDelete } from 'react-icons/md';
+
+const BooksSingleCard = ({ book, handleDelete }) => {
+    return (
+        <div className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 hover:shadow-xl relative'>
+            <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>{book.publishYear}</h2>
+            <h4 className='my-2 text-gray-500'>{book._id}</h4>
+            <div className='flex justify-start items-center gap-x-2'>
+                <PiBookOpenTextLight className='text-red-300 text-2xl' />
+                <h2 className='my-1'>{book.title}</h2>
+            </div>
+            <div className='flex justify-start items-center gap-x-2'>
+                <BiUserCircle className='text-red-300 text-2xl' />
+                <h2 className='my-1'>{book.author}</h2>
+            </div>
+            <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
+                <Link to={`/books/details/${book._id}`}>
+                    <BiInfoCircle className='text-green-800 text-2xl hover:text-black' />
+                </Link>
+                <Link to={`/books/edit/${book._id}`}>
+                    <AiOutlineEdit className='text-yellow-800 text-2xl hover:text-black' />
+                </Link>
+                <button to={`/books/delete/${book._id}`}>
+                    <MdOutlineDelete className='text-red-800 text-2xl hover:text-black' />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default BooksSingleCard
